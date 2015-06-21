bd7a667a7ff5f22064d78cc401e0815c901e0bbc
Jennifer Zhang <jenniferjuliazhang@gmail.com>
2015-06-20 21:35:20 -0700

    Routes to history and index get and post

--- server.py
+++ server.py
@@ -4,7 +4,29 @@ from database import get_db
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
+def index():
+	# Check table, populate if empty
+	# Show two random individuals in the current gen
+
+	return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def decision():
+	# Get id of winner and loser
+	# Compute elo score change of winner and loser
+	# Modify scores in db
+	# If we are at the end of current gen, do all the evolution stuff
+	# Redirect back to the homepage with 2 new individuals
+
+
+@app.route("/history")
+def history():
+	# Table of generation numbers, params, and elo scores
+	# Show images in v2.
+
+	return render_template('history.html')
+
 @app.route("/<text>")
 def index(text=Constants.generation_length):
     return render_template('index.html', text=text)
