19a7302a2a9422d61aa37ad99d2d55011034141a
Anish Athalye <me@anishathalye.com>
2015-06-20 23:57:56 -0700

    Tweak coloring

--- species.py
+++ species.py
@@ -16,6 +16,7 @@ def domains():
         'palette_3': COLOR,
         'palette_4': COLOR,
         'palette_5': COLOR,
+        'colors_limit': [3, 4, 5],
         'h_left_color': range(5),
         'h_mid_color': range(5),
         'h_right_color': range(5),
@@ -37,11 +38,12 @@ def combine(parent_a, parent_b):
 def generate(parameters):
     def color2rgb(color):
         return '#%06x' % color
+    limit = parameters['colors_limit']
     colors = list(map(color2rgb, [parameters['palette_{}'.format(i)] for i in range(1,6)]))
     opacity = parameters['opacity']
-    h_left = '''<rect height="110.0" width="50.0" opacity="{opacity}" fill="%s"/>''' % colors[parameters['h_left_color']]
+    h_left = '''<rect height="110.0" width="50.0" opacity="{opacity}" fill="%s"/>''' % colors[parameters['h_left_color'] % limit]
     h_mid = '''<rect height="33.333333333333336" width="100.0" opacity="{opacity}" fill="%s" x="10.0" y="38.33333333333333"/>''' % colors[parameters['h_mid_color']]
-    h_right = '''<rect height="110.0" width="50.0" opacity="{opacity}" fill="%s" x="60.0"/>''' % colors[parameters['h_right_color']]
+    h_right = '''<rect height="110.0" width="50.0" opacity="{opacity}" fill="%s" x="60.0"/>''' % colors[parameters['h_right_color'] % limit]
     if parameters['h_left_above']:
         h_parts = [h_mid, h_left]
     else:
@@ -64,11 +66,11 @@ def generate(parameters):
           <polygon points="20.0,110.0 90.0,110.0 55.0,40.0" opacity="{opacity}" fill="%s"/>
         </g>
       </g>
-    ''' % (colors[parameters['a_outer_color']], colors[parameters['a_inner_color']])
+    ''' % (colors[parameters['a_outer_color'] % limit], colors[parameters['a_inner_color'] % limit])
     c_bottom = '''<g transform="translate(0,110.0) scale(1,-1)">
             <path opacity="{opacity}" d="M 99.49593469062211,22.671811123913976 A 55.0,55.0 0 1 0 33.95241121992003 105.81337428812076 Z" fill="%s"/>
-          </g>''' % colors[parameters['c_bottom_color']]
-    c_top = '''<path opacity="{opacity}" d="M 99.49593469062211,22.671811123913976 A 55.0,55.0 0 1 0 33.95241121992003 105.81337428812076 Z" fill="%s"/>''' % colors[parameters['c_top_color']]
+          </g>''' % colors[parameters['c_bottom_color'] % limit]
+    c_top = '''<path opacity="{opacity}" d="M 99.49593469062211,22.671811123913976 A 55.0,55.0 0 1 0 33.95241121992003 105.81337428812076 Z" fill="%s"/>''' % colors[parameters['c_top_color'] % limit]
     c_parts = [c_bottom, c_top] if parameters['c_top_above'] else [c_top, c_bottom]
     letter_c = '''
       <g transform="translate(20.0,150.0)">
@@ -77,8 +79,8 @@ def generate(parameters):
         </g>
       </g>
     '''.format(*c_parts)
-    k_top = '''<polygon points="0,110.0 0,0 110.0,0 50.0,110.0" opacity="{opacity}" fill="%s"/>''' % colors[parameters['k_top_color']]
-    k_bottom = '''<polygon points="0,0 0,110.0 110.0,110.0 50.0,0" opacity="{opacity}" fill="%s"/>''' % colors[parameters['k_bottom_color']]
+    k_top = '''<polygon points="0,110.0 0,0 110.0,0 50.0,110.0" opacity="{opacity}" fill="%s"/>''' % colors[parameters['k_top_color'] % limit]
+    k_bottom = '''<polygon points="0,0 0,110.0 110.0,110.0 50.0,0" opacity="{opacity}" fill="%s"/>''' % colors[parameters['k_bottom_color'] % limit]
     k_parts = [k_bottom, k_top] if parameters['k_top_above'] else [k_top, k_bottom]
     letter_k = '''
       <g transform="translate(150.0,150.0)">
